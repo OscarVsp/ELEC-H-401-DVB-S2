d00112ce07439d18fd7a89f15d4e26a95dae7c57
oscar <boscar.vs98@gmail.com>
2021-03-03 10:48:31 +0100

    added figure for the symbols

--- 1 Optimal communication chain over the ideal channel/SymbolMapping.m
+++ 1 Optimal communication chain over the ideal channel/SymbolMapping.m
@@ -1,18 +1,31 @@
+clear; clc;
+
+
 %%%----Parameters----%%%
 
 Nbit = 200;             %Nombre of bits
 Nbps = 4;               %Nombre of bits per symbol
-mod = 'pam';            %Type of modulation ('qam' or 'pam')
+mod = 'qam';            %Type of modulation ('qam' or 'pam')
 
 
 %%%----Bit Generator----%%%
 
 bit_tx = randi(2,Nbit,1)-1;
 
+%fig_bit_tx = figure('Name','bits','NumberTitle','off');
+%plot((0:size(bit_tx)-1),bit_tx);
+%hold;
+
 
 %%%----Mapping----%%%
 
 symb_tx = mapping(bit_tx, Nbps, mod);
+
+fig_symb_tx = figure('Name','Symbols','NumberTitle','off');
+plot(symb_tx,'o'); %mettre les valeurs de références
+hold;
+plot(symb_tx,'.');
+hold;
 
 %%%----Transmitter Filter----%%%
 
