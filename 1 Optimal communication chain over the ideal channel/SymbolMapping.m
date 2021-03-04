4eab83732d595c69bf4ca624e8bdf5316da7c60f
oscar <boscar.vs98@gmail.com>
2021-03-04 16:57:28 +0100

    added noise function and (de)mapping condition CORRECT

--- 1 Optimal communication chain over the ideal channel/SymbolMapping.m
+++ 1 Optimal communication chain over the ideal channel/SymbolMapping.m
@@ -7,21 +7,17 @@ Nbit = 2000;             %Nombre of bits
 Nbps = 4;               %Nombre of bits per symbol
 
 
-
-
 %% Bit Generator
 
 bit_tx = randi(2,Nbit,1)-1;
 
 
-
-
 %% Mapping
 
 if (Nbps > 1)
-    symb_tx = mapping(bit_tx.', bitsPerSymbol, 'qam');
+    symb_tx = mapping(bit_tx, Nbps, 'qam');
 else
-    symb_tx = mapping(bit_tx.', bitsPerSymbol, 'pam');
+    symb_tx = mapping(bit_tx, Nbps, 'pam');
 end
 
 
@@ -41,15 +37,15 @@ fig_signal_tx = figure('Name','signal_tx','NumberTitle','off');plot(signal_rx,'b
 
 %% Receiver Filter
 
-symb_rx = RRC(signal_rx);
+symb_rx = HalfrootNyquistFilter(signal_rx);
 
 
 %% Demapping
 
 if (Nbps > 1)
-    bit_rx = demapping(symb_rx, bitsPerSymbol, 'qam').';
+    bit_rx = demapping(symb_rx, Nbps, 'qam').';
 else
-    bit_rx = demapping(real(symb_rx), bitsPerSymbol, 'pam').';
+    bit_rx = demapping(real(symb_rx), Nbps, 'pam').';
 end
 
 
