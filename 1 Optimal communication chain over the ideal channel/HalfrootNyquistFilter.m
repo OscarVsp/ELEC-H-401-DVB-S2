877afbecea82d0c65c311327cea5225987f405cd
françois verschueren <user.name>
2021-03-04 18:10:48 +0100

    The RRC should be ok: impulse response centered and zeroes crossing at T period

--- 1 Optimal communication chain over the ideal channel/HalfrootNyquistFilter.m
+++ 1 Optimal communication chain over the ideal channel/HalfrootNyquistFilter.m
@@ -11,7 +11,7 @@ function [output_signal] = HalfrootNyquistFilter(input_signal,fs)
 
 beta = 0.3; %Makes the window smoother as beta increases // roll-off factor
 T = 1/(2*(fs/10)); %Sampling period to avoid ISI given by the f_cut (slide 29 p211) not sure about this relation !
-N = 31; % Number of filter samples may be given by the fs
+N = 51; % Number of filter samples may be given by the fs
 
 f_step = fs/N;
 f_max = f_step*(N-1)/2;
@@ -19,15 +19,7 @@ f = linspace(-f_max,f_max,N);
 
 t_step = 1/fs;
 t = (-(N-1)/2:(N-1)/2)*t_step;
-%% filter in time domain
-%Formula given at slide 30
-% h = ( sinc(pi*t/T).*(cos(pi*beta*t/T)) )./( 1 - 4*(beta^2)*(t.^2)/(T^2) ); %Watch out beacause alpha at denom but is it a "coquille 
-% figure(2);
-% title("Impulse response of the raised cosine");
-% 
-% plot(t,h);grid on;
-% hold on;
-% plot(t,h,'.');
+
 %% filter in the frequency domain
 %go with fft and calculated function
 
@@ -55,8 +47,20 @@ for i = 1:length(f)
 end
 
 
-figure(4);plot(f,H); grid on;title("RRC filter window");
+figure(4);plot(f,H,'*'); grid on;title("RRC filter window");
+%% filter in time domain
+%Formula given at slide 30
+% h = ( sinc(pi*t/T).*(cos(pi*beta*t/T)) )./( 1 - 4*(beta^2)*(t.^2)/(T^2) ); %Watch out beacause alpha at denom but is it a "coquille 
+% figure(2);
+% title("Impulse response of the raised cosine");
+% 
+% plot(t,h);grid on;
+% hold on;
+% plot(t,h,'.');
 
+h = ifft(H);
+h= fftshift(h); %shift to center the sinc
+figure(5); plot(t,abs(h)); grid on; title("Impulse response of the raised cosine");
 %% Output
 output_signal = input_signal; %Do the filter
 
