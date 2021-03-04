8af02b941c043fb42357e69c720b3a83322a52c5
françois verschueren <user.name>
2021-03-04 16:53:31 +0100

    Bonne version du filtre en freq

--- 1 Optimal communication chain over the ideal channel/HalfrootNyquistFilter.m
+++ 1 Optimal communication chain over the ideal channel/HalfrootNyquistFilter.m
@@ -11,7 +11,7 @@ function [output_signal] = HalfrootNyquistFilter(input_signal)
 
 beta = 0.3; %Makes the window smoother as beta increases // roll-off factor
 f_cut = 1e6; %Hz Cutoff frequency 
-T = (beta/f_cut)*(1-beta/2); %Sampling period to avoid ISI given by the f_cut (slide 29 p211) not sure about this relation !
+T = 1/(2*f_cut); %Sampling period to avoid ISI given by the f_cut (slide 29 p211) not sure about this relation !
 N = 10000; % Number of filter samples may be given by the fs
 fs = 25*f_cut; % Sampling frequency (rule of thumb for the 10 25 times f_cut)
 
@@ -23,25 +23,40 @@ t_step = 1/fs;
 t = (-(N-1)/2:(N-1)/2)*t_step;
 %% filter in time domain
 %Formula given at slide 30
-h = ( sinc(pi*t/T).*(cos(pi*beta*t/T)) )./( 1 - 4*(beta^2)*(t.^2)/(T^2) ); %Watch out beacause alpha at denom but is it a "coquille 
-figure(2);
-title("Impulse response of the raised cosine");
-
-plot(t,h);grid on;
-hold on;
-plot(t,h,'.');
+% h = ( sinc(pi*t/T).*(cos(pi*beta*t/T)) )./( 1 - 4*(beta^2)*(t.^2)/(T^2) ); %Watch out beacause alpha at denom but is it a "coquille 
+% figure(2);
+% title("Impulse response of the raised cosine");
+% 
+% plot(t,h);grid on;
+% hold on;
+% plot(t,h,'.');
 %% filter in the frequency domain
 %go with fft and calculated function
 
 %via fft
-H = fft(h);
-tailledeH = length(H);
-figure(3);
-title("Root raised cosine window by fft");
-plot(f,abs(H));grid on;
+% H = fft(h);
+% tailledeH = length(H);
+% figure(3);
+% title("Root raised cosine window by fft");
+% plot(f,abs(H));grid on;
 
 %By the course calculation
-%H_course= 
+
+H=zeros(length(f));
+%creating the fucntion by the cases given in slide 30 p212
+lower_bound= (1-beta)/(2*T);
+upper_bound= (1+beta)/(2*T);
+for i = 1:length(f)
+    if abs(f(i)) < lower_bound
+        H(i)=T;
+    elseif (lower_bound <= abs(f(i))) && (abs(f(i)) <= upper_bound )
+        H(i) = (T/2)*( 1 + cos( (pi*T/beta)*(abs(f(i)) - (1-beta)/(2*T))) );
+    end
+end
+
+figure(4);
+title("RRC filter window");
+plot(f,H); grid on;
 %% Output
 output_signal = input_signal; %Do the filter
 
