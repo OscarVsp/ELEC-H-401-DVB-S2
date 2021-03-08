8cc5cd4d5cf5e8875aa002cceea7931e9c1c6c78
françois verschueren <user.name>
2021-03-08 12:22:40 +0100

    réglage du prob matrice 51x51 et check prob des zéros (non résolu)

--- 1 Optimal communication chain over the ideal channel/HalfrootNyquistFilter.m
+++ 1 Optimal communication chain over the ideal channel/HalfrootNyquistFilter.m
@@ -5,13 +5,16 @@ function [filter] = HalfrootNyquistFilter(fs)
 %
 % OUTPUTS:
 % - output_signal : vector of ouput signal
-
+%
+%This function creates the Nyquist filter that will be used to maximize the
+%SNR. The sampling frequency has to be the same as the symbols to convolve
+%correctly with this filter.
 
 %% filter parameters
 
 beta = 0.3; %Makes the window smoother as beta increases // roll-off factor
 T = 1/(2*(fs/10)); %Sampling period to avoid ISI given by the f_cut (slide 29 p211) not sure about this relation !
-N = 51; % Number of filter samples may be given by the fs
+N = 201; % Number of filter samples may be given by the fs
 
 f_step = fs/N;
 f_max = f_step*(N-1)/2;
@@ -32,7 +35,7 @@ t = (-(N-1)/2:(N-1)/2)*t_step;
 
 %By the course calculation
 
-H=zeros(length(f));
+H=zeros(1,length(f));
 %creating the fucntion by the cases given in slide 30 p212
 lower_bound= (1-beta)/(2*T);
 upper_bound= (1+beta)/(2*T);
@@ -47,7 +50,7 @@ for i = 1:length(f)
 end
 
 
-figure(4);plot(f,H,'*'); grid on;title("RRC filter window");
+%figure(4);plot(f,H,'*'); grid on;title("RC filter window");
 %% filter in time domain
 %Formula given at slide 30
 % h = ( sinc(pi*t/T).*(cos(pi*beta*t/T)) )./( 1 - 4*(beta^2)*(t.^2)/(T^2) ); %Watch out beacause alpha at denom but is it a "coquille 
@@ -57,12 +60,17 @@ figure(4);plot(f,H,'*'); grid on;title("RRC filter window");
 % plot(t,h);grid on;
 % hold on;
 % plot(t,h,'.');
+G = sqrt(H); %Root of the filter to implement it at transmiter and receiver
+figure(6);plot(f,G,'*'); grid on;title("RRC filter window");
+g = ifft(G);
+g= fftshift(g); %shift to center the sinc
 
-h = ifft(H);
-h= fftshift(h); %shift to center the sinc
-figure(5); plot(t,real(h)); grid on; title("Impulse response of the raised cosine");
+h= ifft(H);
+h= fftshift(h);
+figure(5); plot(t,real(g));grid on; title("Impulse response of the raised cosine"); figure(4); plot(t,real(h)); grid on; title("Impulse response of the raised cosine");
+%ça passe super près de 0 mais savoir si c'est négligeable :/ ?
 %% Output
-filter = h; %Output the filter, the convolution is done on the main function so that we only compute de filter once at the start
+filter = g; %Output the filter, the convolution is done on the main function so that we only compute de filter once at the start
 
 end
 
