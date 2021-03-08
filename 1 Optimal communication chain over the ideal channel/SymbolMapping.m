663f1fb160bf570d4a7328e7c1f24b68e36b5167
françois verschueren <user.name>
2021-03-08 21:20:21 +0100

    Changements pour la convolution tjrs des probs

--- 1 Optimal communication chain over the ideal channel/SymbolMapping.m
+++ 1 Optimal communication chain over the ideal channel/SymbolMapping.m
@@ -3,19 +3,19 @@ clear all; clc; close all;
 
 %% Parameters
 
-image_tx = 'cp.png'; %Only 3x8bits images
-[bit_tx,nbl,nbc,nbd] = ImageToBits(image_tx);
+% image_tx = 'cp.png'; %Only 3x8bits images
+% [bit_tx,nbl,nbc,nbd] = ImageToBits(image_tx);
 
 Nbit = 2000;
 Nbps = 4;               %Nombre of bits per symbol
-M = 4;       %Upsampling factor
+M = 128;       %Upsampling factor
 f_cut = 1e6; %Hz Cutoff frequency
 fs = 10*f_cut; % Sampling frequency (rule of thumb for the 10 25 times f_cut)
-EbNo = 0.1; %Energy of one by over the PSD of the noise ratio (in dB)
+EbNo =50; %Energy of one by over the PSD of the noise ratio (in dB)
 
 %% Bit Generator
 
-%bit_tx = randi(2,1,Nbit)-1; 
+bit_tx = randi(2,1,Nbit)-1; 
 Nbit = length(bit_tx);
 
 %% Mapping
@@ -35,26 +35,28 @@ upsampled_symb_tx = UpSampling(symb_tx,Nbit,Nbps,M);
 
 filter = HalfrootNyquistFilter(fs); %So we compute de filter only 1 time as this is the same
 
-%signal_tx = conv(upsampled_symb_tx,filter); %doesn't work for now, bcs
-%filter is not a vector, it's a 51x51 complexe matrix
+signal_tx = conv(upsampled_symb_tx,filter);%Convolution of the signal with the filter
 
-signal_tx = upsampled_symb_tx;
+N_filter = length(filter);
+signal_tx = signal_tx(floor(N_filter/2):(length(signal_tx)-floor(N_filter/2)-1) ); %Removing unecessary parts due to convolution (conv length = N + M -1 and we need to stay at M)
+figure(7); stem(abs(signal_tx)); title("transmited signal")
+%signal_tx = upsampled_symb_tx;
 
 
 %% Transmission Channel
 
-signal_rx = NoiseAddition(signal_tx,EbNo,fs,Nbit);      %With Noise
-%signal_rx = signal_tx;                              %Without Nosie
+%signal_rx = NoiseAddition(signal_tx,EbNo,fs,Nbit);      %With Noise
+signal_rx = signal_tx;                              %Without Nosie
 
 %fig_signal_tx = figure('Name','signal_tx','NumberTitle','off');plot(signal_rx,'b.');grid on;hold on;plot(signal_tx,'ro');
 
 
 
 %% Receiver Filter
-
-%upsampled_symb_rx = conv(signal_rx,filter); %same as transmitter
-
-upsampled_symb_rx = signal_rx;
+matched_filter = flip(filter); %Get the time reversal of the filter g(-t) 
+upsampled_symb_rx = conv(signal_rx,matched_filter); %Matched filter convolved with the signal
+upsampled_symb_rx = upsampled_symb_rx(floor(N_filter/2):(length(upsampled_symb_rx)-floor(N_filter/2)-1) );
+%upsampled_symb_rx = signal_rx;
 
 %% Downsampling
 
@@ -74,5 +76,5 @@ end
 
 ErrorRatio = ErrorCalculator(bit_rx,bit_tx)
 
-image_rx = BitsToImage(bit_rx,nbl,nbc,nbd);
-figure('name',"BER = "+num2str(ErrorRatio,4));image(image_rx);
+%image_rx = BitsToImage(bit_rx,nbl,nbc,nbd);
+%figure('name',"BER = "+num2str(ErrorRatio,4));image(image_rx);
