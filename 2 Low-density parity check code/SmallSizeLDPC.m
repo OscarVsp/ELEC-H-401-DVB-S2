e14a8868a81e13efb05277a5853f0813c5307925
françois verschueren <user.name>
2021-04-19 20:57:35 +0200

    On avance mais y a encore des kwaks dans les for : manque le for d'optimisation

--- 2 Low-density parity check code/SmallSizeLDPC.m
+++ 2 Low-density parity check code/SmallSizeLDPC.m
@@ -28,7 +28,7 @@ d= [ 0 0 1 0 1]
 
 u = mod(d*G,2)
 %% Step 0 
-r = mod(u+[0 0 0 1 0 0 0 0 0 0],2) %received signal with 1 bit error
+r = mod(u+[0 0 0 0 0 0 0 1 0 0],2) %received signal with 1 bit error
 
 s1 = mod(u*H_true',2) %syndrome 0 if no error 
 s2= mod(r*H_true',2) % different from zero if error
@@ -36,14 +36,23 @@ s2= mod(r*H_true',2) % different from zero if error
 %% Step 1 
 % NE PREND PAS EN COMPTE LES VALEURS DE CHAQUE NOEUD
 %Receiving messages from check nodes and vote the result 
-vote = r;
-for i=1:length(s2)
-    
-    if s2(i) == 1 %changing corresponding bits if check node equals 1 from syndrome
-        vote = vote+ mod(r + H_true(i,:),2)  
-    else
-        vote = vote + r
+[m n] = size(H_true);
+vote_result = zeros(1,n);
+
+for i=1:n %for each variable node
+    check_node = H_true(:,i); %taking the link between check and variable nodes
+    vote = r(i);
+    count =1;%Because the received vector is taken for the majority vote
+    for j=1:m %for each check node for this variable node
+        if check_node(j) == 1 %decision of the check node needed
+            count =count+ 1
+            if s2(j) == 1 %changing corresponding bits if check node equals 1 from syndrome
+                vote = vote+ mod(r(i) + 1,2)  %inversing the value because assumed to be an error
+            else
+                vote = vote + r(i)
+            end
+        end
     end
+    vote_result(i) = round(vote/count) %Vote
 end
-%Vote
-result = round(vote/(length(s2)+1))
+
