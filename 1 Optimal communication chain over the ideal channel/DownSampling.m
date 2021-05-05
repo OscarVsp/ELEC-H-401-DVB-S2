27bcfe62dbf850519770401ccad85a02dbfe035c
françois verschueren <user.name>
2021-05-05 16:27:00 +0200

    Finishing to see effects of sync errors and creation of a new live script for the sync algos

--- 1 Optimal communication chain over the ideal channel/DownSampling.m
+++ 1 Optimal communication chain over the ideal channel/DownSampling.m
@@ -1,22 +1,22 @@
-function [symb_rx] = DownSampling(upsampled_symb_rx,Nbit,Nbps,M)
+function [symb_rx] = DownSampling(upsampled_symb_rx,Nbit,Nbps,M,varargin)
 
 % INPUTS:
 % - symb_tx : vector of input symbol to upsample
 % - Nbit : nomber of bit
 % - Nbps : nomber of symbol per bits
 % - M : umsampling factor
-%
+% - varargin : only one value : the time shift
 % OUTPUTS:
 % - upsampled_symb_tx : vector of ouput symbol upsampled
-
+time_shift=0;
+if length(varargin) ==1
+    time_shift=varargin{1};
+end
 
 symb_rx = zeros(1,Nbit/Nbps);
 for i = 1:Nbit/Nbps
-    symb_rx(i)=upsampled_symb_rx(1+M*(i-1));
+    symb_rx(i)=upsampled_symb_rx(1+time_shift+M*(i-1));
 end
 
 
 end
-
-
-
--- 3 Time and frequency synchronisation/Add_CFO_and_phase_shift.m
+++ 3 Time and frequency synchronisation/Add_CFO_and_phase_shift.m
@@ -1,4 +1,4 @@
-function [corrupted_signal] = Add_CFO_and_phase_shift(r,CFO,fsamp)
+function [corrupted_signal,phase_shift] = Add_CFO_and_phase_shift(r,CFO,fsamp)
 %This function adds CFO and phase shift errors. It has to be implemented
 %just before the matched filter in the communication channel.
 %   r is the signal affected by the errors
@@ -6,8 +6,8 @@ function [corrupted_signal] = Add_CFO_and_phase_shift(r,CFO,fsamp)
 %   fc is the carrier frequency
 %   phase_shift is the phase shift between 0 and 2*pi
 
-%phase_shift = rand(1)*2*pi;
-phase_shift = 0;
+phase_shift = rand(1)*2*pi;
+%phase_shift = 0;
 N = length(r); n = 0:N-1;
 T = 1/fsamp;
 corrupted_signal = r.*exp(1j*(2*pi*CFO*T.*n +phase_shift));
