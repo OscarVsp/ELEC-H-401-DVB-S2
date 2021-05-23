94636371c0ec25f33a2e5860563b291b9f777191
françois verschueren <user.name>
2021-05-23 15:52:31 +0200

    Fin de l'algo DA frame acq

--- 3 Time and frequency synchronisation/Add_sync_errors.m
+++ 3 Time and frequency synchronisation/Add_sync_errors.m
@@ -12,13 +12,13 @@ function [corrupted_signal,phase_shift,time_shift] = Add_sync_errors(r,CFO,fsamp
 eps = (rand(1)-0.5); % values of eps in [-0.5,0.5] time shift is eps*T (symbol duration)
 time_shift= round(eps*(M-1)); % We can have a time shift up to 1 symbol in indices
 %time_shift=-4;
-phase_shift = rand(1)*2*pi;
-%phase_shift = 0;
+%phase_shift = rand(1)*2*pi;
+phase_shift = 0;
 N = length(r); n = 0:N-1;
 T = 1/fsamp;
 
-%corrupted_signal = r;
+corrupted_signal = r;
 corrupted_signal = r.*exp(1j*(2*pi*CFO*T.*n +phase_shift)); %multiplication by the complex exponential (CFO and phase shift)
-corrupted_signal = circshift(corrupted_signal,time_shift); %Time shift represented by a circular shift
+%corrupted_signal = circshift(corrupted_signal,time_shift); %Time shift represented by a circular shift
 end
 
--- 3 Time and frequency synchronisation/Da_frame_acq.m
+++ 3 Time and frequency synchronisation/Da_frame_acq.m
@@ -1,4 +1,4 @@
-function [symb_out,n_hat, df_hat] = Da_frame_acq(y,pilot,data_symbol_length,K,T_symb,Nbps,Nbit)
+function [symb_out,n_hat, df_hat,Dk] = Da_frame_acq(y,pilot,data_symbol_length,K,T_symb,Nbps,Nbit)
 
 %This function gets the ToA, the CFO and the time shift of the received signal.
 %The algorithm uses the differential cross correlation and removes the pilots.
@@ -23,24 +23,34 @@ df_hat = zeros(1,floor(seq_length/codeblock_length)); %estimated phase shift of 
 symb_out = zeros(1,Nbit/Nbps ); %number of symbols at the output
 for i= 1:codeblock_length:seq_length-codeblock_length %-window_ratio*codeblock_length %for each packet with a pilot
 	indices = i:i+window_ratio*codeblock_length-1;	
-	window_y = y(indices);
-	Dk = zeros(K,length(window_y));
-	for n = 1:length(window_y)
+	window_y = [y(indices) zeros(1,pilot_length)];
+	Dk = zeros(K,length(indices));
+	for n = 1:length(indices)
 		for k=1:K
 			for l=k+1:pilot_length
-				 Dk(k,n) = Dk(k,n) + conj(y(n+l))*pilot(l)*conj( conj(y(n+l-k)*pilot(l-k) ));
+				 Dk(k,n) = Dk(k,n) + (conj(y(n+l))*pilot(l) )*conj( conj(window_y(n+l-k))*pilot(l-k) );
 			end
 			Dk(k,n) = Dk(k,n)/(pilot_length-k);
 		end
 	end
 	[~ , n_arg_max] = max(sum(Dk)); %sum of all the columns gives n_hat
 	n_hat(1+(i-1)/codeblock_length) = indices(n_arg_max); % Get the good index in the window 
-        df_hat(1+(i-1)/codeblock_length) = -sum(angle(Dk(:,n_arg_max))./([1:K]'*2*pi*T_symb) ); %Get the phase value
+        df_hat(1+(i-1)/codeblock_length) = -sum(unwrap(angle(Dk(:,n_arg_max)))./([1:K]') )/K; %Get the phase value
         
         n_max_y =  indices(n_arg_max);
-        data = y(n_max_y+pilot_length:n_max_y + pilot_length +data_symbol_length-1);
-        data = data*exp(1j*df_hat(1+(i-1)/codeblock_length)*2*pi*T_symb); %correcting the data from the CFO
-        data = data*exp( 1j*2*pi*T_symb*(df_hat(1+(i-1)/codeblock_length) - df_hat( (i-1+codeblock_length)/codeblock_length)) ); %phase interpolation
+        data = y(n_max_y+pilot_length+1:n_max_y + pilot_length +data_symbol_length);  %phase interpolation
+        n=i+pilot_length:codeblock_length + i-1; % taking the good indices where the CFO was
+        
+        if i ==1
+            phase_interp = 0;
+            phase_diff =0;
+        else
+            phase_diff = df_hat(1+(i-1)/codeblock_length)-df_hat( (i-1+codeblock_length)/codeblock_length -1);
+            phase_interp= df_hat( (i-1+codeblock_length)/codeblock_length -1) :phase_diff/length(data): df_hat(1+(i-1)/codeblock_length)-phase_diff/length(data);
+        end
+        
+        data = data.*exp(-1j*df_hat(1)*n-1j*phase_interp); %get rid of the CFO by multiplying back the complex exponential
+        
         symb_out(1+(i-1)*data_symbol_length/codeblock_length:(i+codeblock_length-1)*data_symbol_length/codeblock_length) = data;
 end
 
